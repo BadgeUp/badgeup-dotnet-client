976a68c4e219866fad2e5cb848a6bf402df66917
Anarios <selivano.d@gmail.com>
2018-03-01 14:27:12 +0300

    Include achievements, awards and criteria properties for GetProgress (#19)

--- BadgeUpClient/ResourceClients/ProgressClient.cs
+++ BadgeUpClient/ResourceClients/ProgressClient.cs
@@ -20,11 +20,24 @@ namespace BadgeUp.ResourceClients
 		/// Retrieves a list of achievements and the completion a subject has towards the achievements. The returned list will also contain earned achievement records when present.
 		/// </summary>
 		/// <param name="subjectId">Unique subject the progress is associated with</param>
+		/// <param name="includeAchievements">Include related achievements</param>
+		/// <param name="includeCriteria">Include related criteria</param>
+		/// <param name="includeAwards">Include related awards</param>
 		/// <returns></returns>
-		public async Task<List<Progress>> GetProgress(string subjectId)
+		public async Task<List<Progress>> GetProgress(string subjectId, bool includeAchievements = false, bool includeCriteria = false, bool includeAwards = false)
 		{
 			var query = new HttpQuery();
 			query.Add("subject", subjectId);
+
+			//Including criteria or awards requires also including achievements
+			if (includeAwards || includeCriteria)
+				includeAchievements = true;
+			if(includeAchievements)
+				query.Add("include", "achievement");
+			if (includeCriteria)
+				query.Add("include", "criterion");
+			if (includeAwards)
+				query.Add("include", "award");
 			return await this.m_httpClient.GetAll<Progress>(ENDPOINT, query:query.ToString());
 		}
 	}
