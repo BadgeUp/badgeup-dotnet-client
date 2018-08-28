f5d4c5179717b0bbf2c0697d937191cd7c13f24c
Stanislav Nedelchev <stan@stannedelchev.net>
2018-08-28 11:20:26 +0300

    [breaking change] Move ApplicationMeta to BadgeUp.Types

---
+++ BadgeUpClient/Types/ApplicationMeta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadgeUp.Types
+{
+	public class ApplicationMeta : Meta
+	{
+		public bool Demo { get; set; }
+	}
+}
